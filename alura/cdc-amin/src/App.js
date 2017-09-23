afb5a8384eb515b4fe2d1af905bf0cf0a0721283
ramos <rdiego26@gmail.com>
2017-09-23 15:34:53 -0300

    ISOLATE table and form .

--- alura/cdc-amin/src/App.js
+++ alura/cdc-amin/src/App.js
@@ -1,81 +1,9 @@
 import React, { Component } from 'react';
-import CustomInput from './components/CustomInput';
-import CustomSubmit from './components/CustomSubmit';
+import {AuthorTable, AuthorForm} from './Author';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.sendForm = this.sendForm.bind(this);
-	  this.setName = this.setName.bind(this);
-	  this.setEmail = this.setEmail.bind(this);
-    this.setPassword = this.setPassword.bind(this);
-    this.state = {
-      list: [],
-      name: '',
-      email: '',
-      password: ''
-    };
-  }
-
-  componentWillMount() {
-    $.ajax({
-      url: 'http://cdc-react.herokuapp.com/api/autores',
-      dataType: 'json',
-      success: function(data) {
-        this.setState({
-          list: data
-        });
-      }.bind(this)
-    });
-  }
-
-  sendForm(event) {
-    event.preventDefault();
-
-    let newAuthor = {
-      nome: this.state.name,
-      senha: this.state.password,
-      email: this.state.email
-    };
-
-    $.ajax({
-      url: 'http://cdc-react.herokuapp.com/api/autores',
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'POST',
-      data: JSON.stringify(newAuthor),
-      success: function(response) {
-        this.setState({
-          list: response
-        });
-      }.bind(this),
-      error: function(response) {
-
-      }
-    });
-  }
-
-  setName(event) {
-    this.setState({
-      name: event.target.value
-    });
-  }
-
-  setEmail(event) {
-	  this.setState({
-		  email: event.target.value
-	  });
-  }
-
-  setPassword(event) {
-	  this.setState({
-		  password: event.target.value
-	  });
-  }
 
   render() {
     return (
@@ -102,46 +30,11 @@ class App extends Component {
             <h1>Cadastro de Autores</h1>
           </div>
           <div className="content" id="content">
-            <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.sendForm} method="POST">
 
-                <CustomInput id="name" name="name" label="Nome" value={this.state.name}
-                             type="text" onChange={this.setName} />
+            <AuthorForm />
 
-                <CustomInput id="email" name="email" label="E-mail" value={this.state.email}
-                             type="email" onChange={this.setEmail} />
+            <AuthorTable />
 
-                <CustomInput id="password" name="password" label="Senha" value={this.state.password}
-                             type="password" onChange={this.setPassword} />
-
-
-                <CustomSubmit label="Gravar" />
-
-              </form>
-
-            </div>
-            <div>
-              <table className="pure-table">
-                <thead>
-                <tr>
-                  <th>Nome</th>
-                  <th>email</th>
-                </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.list.map((author) => {
-                      return (
-                        <tr key={author.id}>
-                          <td>{author.nome}</td>
-                          <td>{author.email}</td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table>
-            </div>
           </div>
         </div>
 
